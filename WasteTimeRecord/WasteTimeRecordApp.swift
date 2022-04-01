0447a29794eadc55fe90bf213a28dd4cd82e02b3
KimDaeSeong <KimDaeSeong8721@github.com>
2022-04-01 22:09:54 +0900

    Initial Commit

---
+++ WasteTimeRecord/WasteTimeRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasteTimeRecordApp.swift
+//  WasteTimeRecord
+//
+//  Created by DaeSeong on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct WasteTimeRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
